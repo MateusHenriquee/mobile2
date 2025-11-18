5c731646f56683187b492b5787e55d8a44de1886
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2025-11-18 09:50:34 -0300

    cu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-const { useState } = require("react");
+import { useState } from "react";
 
-function App() {
-  const [message, setMessage] = useState ('hello world')
+function App(){
+  const [message, setMessage] = useState('hello world');
+  return(
+    <div>
+      <h1>{message}</h1>
+      <button onClick={
+        ()=>{setMessage('olá fui clicado');}
+      }></button>
+
+    </div>
+  )
 }  
+export default App
